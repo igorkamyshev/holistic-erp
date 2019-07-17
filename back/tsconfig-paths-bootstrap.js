b45b48c9870159355cc7557ec0a829a5f3d667a7
Igor Kamyshev <igor@kamyshev.me>
2019-07-17 11:18:18 +0300

    fix: fix oath for aliases

--- back/tsconfig-paths-bootstrap.js
+++ back/tsconfig-paths-bootstrap.js
@@ -3,7 +3,7 @@ const tsConfigPaths = require('tsconfig-paths')
 const { paths } = require('./tsconfig.json').compilerOptions
 const { resolve } = require('path')
 
-const baseUrl = resolve(__dirname, './dist')
+const baseUrl = resolve(__dirname, '../dist')
 tsConfigPaths.register({
   baseUrl,
   paths,
